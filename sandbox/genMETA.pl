8376675e1d9acf8ab14d136451e14635412f0420
H.Merijn Brand <merijn@nb09.procura.nl>
2008-10-21 17:38:45 +0200

    Add genMETA.yml

---
+++ sandbox/genMETA.pl
@@ -0,0 +1,87 @@
+#!/pro/bin/perl
+
+use strict;
+use warnings;
+
+use Getopt::Long qw(:config bundling nopermute);
+my $check = 0;
+my $opt_v = 0;
+GetOptions (
+    "c|check"		=> \$check,
+    "v|verbose:1"	=> \$opt_v,
+    ) or die "usage: $0 [--check]\n";
+
+my $version;
+open my $pm, "<", "SCCS.pm" or die "Cannot read SCCS.pm";
+while (<$pm>) {
+    m/^.VERSION\s*=\s*"?([-0-9._]+)"?\s*;\s*$/ or next;
+    $version = $1;
+    last;
+    }
+close $pm;
+
+my @yml;
+while (<DATA>) {
+    s/VERSION/$version/o;
+    push @yml, $_;
+    }
+
+if ($check) {
+    use YAML::Syck;
+    use Test::YAML::Meta::Version;
+    my $h;
+    my $yml = join "", @yml;
+    eval { $h = Load ($yml) };
+    $@ and die "$@\n";
+    $opt_v and print Dump $h;
+    my $t = Test::YAML::Meta::Version->new (yaml => $h);
+    $t->parse () and die join "\n", $t->errors, "";
+
+    use Parse::CPAN::Meta;
+    eval { Parse::CPAN::Meta::Load ($yml) };
+    $@ and die "$@\n";
+
+    print "Checking if 5.006 is still OK as minimal version for examples\n";
+    use Test::MinimumVersion;
+    # All other minimum version checks done in xt
+    all_minimum_version_ok ("5.006", { paths => [ "examples" ]});
+    }
+elsif ($opt_v) {
+    print @yml;
+    }
+else {
+    my @my = glob <*/META.yml>;
+    @my == 1 && open my $my, ">", $my[0] or die "Cannot update META.yml|n";
+    print $my @yml;
+    close $my;
+    chmod 0644, glob <*/META.yml>;
+    }
+
+__END__
+--- #YAML:1.1
+name:              VCS::SCCS
+version:           VERSION
+abstract:          OO Interface to SCCS files
+license:           perl
+author:              
+    - H.Merijn Brand <h.merijn@xs4all.nl>
+generated_by:      Author
+distribution_type: module
+provides:
+    Data::Peek:
+        file:      SCCS.pm
+        version:   VERSION
+requires:     
+    perl:          5.006
+    Carp:          0
+    POSIX:         0
+    File::Spec:    0
+build_requires:
+    perl:          5.006
+    Test::Harness: 0
+    Test::More:    0
+resources:
+    license:       http://dev.perl.org/licenses/
+meta-spec:
+    version:       1.4
+    url:           http://module-build.sourceforge.net/META-spec-v1.4.html
