92cfaac8bee8c3f3ff1a2cc2836d617d27fe01c8
H.Merijn Brand <merijn@lx09.procura.nl>
2011-09-07 08:46:37 +0200

    More cross-checks for META data

--- sandbox/genMETA.pl
+++ sandbox/genMETA.pl
@@ -11,67 +11,30 @@ GetOptions (
     "v|verbose:1"	=> \$opt_v,
     ) or die "usage: $0 [--check]\n";
 
-my $version;
-open my $pm, "<", "SCCS.pm" or die "Cannot read SCCS.pm";
-while (<$pm>) {
-    m/^.VERSION\s*=\s*"?([-0-9._]+)"?\s*;\s*$/ or next;
-    $version = $1;
-    last;
-    }
-close $pm;
+use lib "sandbox";
+use genMETA;
+my $meta = genMETA->new (
+    from    => "SCCS.pm",
+    verbose => $opt_v,
+    );
 
-my @yml;
-while (<DATA>) {
-    s/VERSION/$version/o;
-    push @yml, $_;
-    }
+$meta->from_data (<DATA>);
 
 if ($check) {
-    print STDERR "Check required and recommended module versions ...\n";
-    BEGIN { $V::NO_EXIT = $V::NO_EXIT = 1 } require V;
-    my %vsn = map { m/^\s*([\w:]+):\s+([0-9.]+)$/ ? ($1, $2) : () } @yml;
-    delete @vsn{qw( perl version )};
-    for (sort keys %vsn) {
-	$vsn{$_} eq "0" and next;
-	my $v = V::get_version ($_);
-	$v eq $vsn{$_} and next;
-	printf STDERR "%-35s %-6s => %s\n", $_, $vsn{$_}, $v;
-	}
-
-    print STDERR "Checking generated YAML ...\n";
-    use YAML::Syck;
-    use Test::YAML::Meta::Version;
-    my $h;
-    my $yml = join "", @yml;
-    eval { $h = Load ($yml) };
-    $@ and die "$@\n";
-    $opt_v and print Dump $h;
-    my $t = Test::YAML::Meta::Version->new (yaml => $h);
-    $t->parse () and die join "\n", $t->errors, "";
-
-    use Parse::CPAN::Meta;
-    eval { Parse::CPAN::Meta::Load ($yml) };
-    $@ and die "$@\n";
-
-    print "Checking if 5.006 is still OK as minimal version for examples\n";
-    use Test::MinimumVersion;
-    # All other minimum version checks done in xt
-    all_minimum_version_ok ("5.006", { paths => [ "examples" ]});
+    $meta->check_encoding ();
+    $meta->check_required ();
+    $meta->check_minimum ([ "examples" ]);
     }
 elsif ($opt_v) {
-    print @yml;
+    $meta->print_yaml ();
     }
 else {
-    my @my = glob <*/META.yml>;
-    @my == 1 && open my $my, ">", $my[0] or die "Cannot update META.yml\n";
-    print $my @yml;
-    close $my;
-    chmod 0644, glob <*/META.yml>;
+    $meta->fix_meta ();
     }
 
 __END__
 --- #YAML:1.0
-name:                    VCS::SCCS
+name:                    VCS-SCCS
 version:                 VERSION
 abstract:                OO Interface to SCCS files
 license:                 perl
@@ -89,7 +52,7 @@ requires:
     POSIX:               0
     File::Spec:          0
 recommends:     
-    perl:                5.012003
+    perl:                5.014001
 configure_requires:
     ExtUtils::MakeMaker: 0
 test_requires:
