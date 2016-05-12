74b482c8ea4903c0186cebdff66f9c4d792efceb
H.Merijn Brand - Tux <h.m.brand@xs4all.nl>
2016-05-12 11:52:15 +0200

    Tested with perl 5.24.0
    • Versions
    • Merge META works

--- sandbox/genMETA.pl
+++ sandbox/genMETA.pl
@@ -58,7 +58,7 @@ test_requires:
     Test::More:          0
     Test::NoWarnings:    0
 test_recommends:
-    Test::More:          1.001014
+    Test::More:          1.302015
 resources:
     license:             http://dev.perl.org/licenses/
     repository:          https://github.com/Tux/VCS-SCCS
