eacbb84a08f1380d96cdbdb95e5f398112753e71
H.Merijn Brand - Tux <h.m.brand@xs4all.nl>
2019-04-28 14:52:54 +0200

    versions

--- sandbox/genMETA.pl
+++ sandbox/genMETA.pl
@@ -58,7 +58,7 @@ test_requires:
     Test::More:          0
     Test::NoWarnings:    0
 test_recommends:
-    Test::More:          1.302162
+    Test::More:          1.302164
 resources:
     license:             http://dev.perl.org/licenses/
     repository:          https://github.com/Tux/VCS-SCCS
